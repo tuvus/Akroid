f8546defec29ea1ee5a603f38367089ce12b780a
Oskar Niesen <oskar-github@niesens.com>
2024-07-05 15:04:24 -0500

    Created an EventChainBuilder to help with the Campaing text

---
+++ Assets/Battle/Campaign/EventChainBuilder.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using static CommunicationEvent;
+
+/// <summary>
+/// EventChainBuilder handles long sequences of events and communcationEvents by creating a list and putting them together once built.
+/// </summary>
+public class EventChainBuilder {
+    private List<object> events = new List<object>();
+    private class CommunicationEventHolder {
+        public FactionCommManager commManager;
+        public Faction reciever;
+        public string text;
+        public float delay;
+
+        public CommunicationEventHolder(FactionCommManager commManager, Faction reciever, string text, float delay) {
+            this.commManager = commManager;
+            this.reciever = reciever;
+            this.text = text;
+            this.delay = delay;
+        }
+    }
+
+    public void AddCommEvent(FactionCommManager commManager, Faction reciever, string text, float delay = 0f) {
+        events.Add(new CommunicationEventHolder(commManager, reciever, text, delay));
+    }
+
+    public void AddEvent(EventCondition eventCondition) {
+        events.Add(eventCondition);
+    }
+
+    /// <summary>
+    /// Builds the EventChain with one final multi-option CommunicationEvent.
+    /// </summary>
+    public Action Build(EventManager eventManager, FactionCommManager commManager, Faction reciever, string text, CommunicationEventOption[] options, float delay = 0f) {
+        return Build(eventManager, () => commManager.SendCommunication(new CommunicationEvent(reciever.GetFactionCommManager(), text, options, true), delay));
+    }
+
+    /// <summary>
+    /// Finalizes constructing the event chain and returns a function to fire it off.
+    /// </summary>
+    public Action Build(EventManager eventManager) {
+        return Build(eventManager, () => { });
+    }
+
+    /// <summary>
+    /// Builds the event chain in reverse order since we need to know the previous action to call.
+    /// </summary>
+    private Action Build(EventManager eventManager, Action lastAction) {
+        for (int i = events.Count - 2; i >= 0; i--) {
+            if (events[i].GetType() == typeof(CommunicationEventHolder)) {
+                CommunicationEventHolder communicationEvent = (CommunicationEventHolder)events[i];
+                lastAction = () => communicationEvent.commManager.SendCommunication(communicationEvent.reciever, communicationEvent.text, (communicationEvent) =>  lastAction(), communicationEvent.delay);
+            } else {
+                EventCondition eventCondition = (EventCondition)events[i];
+                lastAction = () => eventManager.AddEvent(eventCondition, lastAction);
+            }
+        }
+        return lastAction;
+    }
+
+}
--- Assets/Battle/Campaign/EventCondition.cs
+++ Assets/Battle/Campaign/EventCondition.cs
@@ -8,40 +8,72 @@ public class EventCondition {
         Wait,
         SelectUnit,
         SelectFleet,
+        Pan,
+        Zoom,
         Predicate,
     }
 
     public ConditionType conditionType { get; protected set; }
     public Unit unitToSelect { get; protected set; }
     public Fleet fleetToSelect { get; protected set; }
-    public float timeToWait { get; protected set; }
+    public float floatValue { get; protected set; }
+    public float floatValue2 { get; protected set; }
+    public Vector2 postionValue { get; protected set; }
     public Predicate<EventManager> predicate { get; protected set; }
 
-    public EventCondition(float timeToWait) {
-        this.conditionType = ConditionType.Wait;
-        this.timeToWait = timeToWait;
+    private EventCondition() {
+        // No extenal instantiation allowed
     }
 
-    public EventCondition(Unit unitToSelect) {
-        this.conditionType = ConditionType.SelectUnit;
-        this.unitToSelect = unitToSelect;
+    public static EventCondition WaitEvent(float timeToWait) {
+        EventCondition condition = new EventCondition();
+        condition.conditionType = ConditionType.Wait;
+        condition.floatValue = timeToWait;
+        return condition;
     }
 
-    public EventCondition(Fleet fleetToSelect) {
-        this.conditionType = ConditionType.SelectFleet;
-        this.fleetToSelect = fleetToSelect;
+    public static EventCondition SelectUnitEvent(Unit unitToSelect) {
+        EventCondition condition = new EventCondition();
+        condition.conditionType = ConditionType.SelectUnit;
+        condition.unitToSelect = unitToSelect;
+        return condition;
     }
 
-    public EventCondition(Predicate<EventManager> predicate) {
-        this.conditionType = ConditionType.Predicate;
-        this.predicate = predicate;
+    public static EventCondition SelectFleetEvent(Fleet fleetToSelect) {
+        EventCondition condition = new EventCondition();
+        condition.conditionType = ConditionType.SelectFleet;
+        condition.fleetToSelect = fleetToSelect;
+        return condition;
+    }
+
+    public static EventCondition PanEvent(float distanceToPan) {
+        EventCondition condition = new EventCondition();
+        condition.conditionType = ConditionType.Pan;
+        condition.floatValue = distanceToPan;
+        condition.floatValue2 = 0;
+        return condition;
+    }
+
+    public static EventCondition ScrollEvent(float scrollTo) {
+        EventCondition condition = new EventCondition();
+        condition.conditionType = ConditionType.Pan;
+        condition.floatValue = scrollTo;
+        condition.floatValue2 = LocalPlayer.Instance.GetInputManager().GetCamera().orthographicSize;
+        return condition;
+    }
+
+    public static EventCondition PredicateEvent(Predicate<EventManager> predicate) {
+        EventCondition condition = new EventCondition();
+        condition.conditionType = ConditionType.Predicate;
+        condition.predicate = predicate;
+        return condition;
     }
 
     public bool CheckCondition(EventManager eventManager, float deltaTime) {
         switch (conditionType) {
             case ConditionType.Wait:
-                timeToWait -= deltaTime;
-                if (timeToWait <= 0)
+                floatValue -= deltaTime;
+                if (floatValue <= 0)
                     return true;
                 break;
             case ConditionType.SelectUnit:
@@ -54,6 +86,26 @@ public class EventCondition {
                 if (eventManager.playerGameInput.GetSelectedUnits().fleet == fleetToSelect
                     && eventManager.playerGameInput.GetSelectedUnits().groupType == SelectionGroup.GroupType.Fleet) {
                     return true;
+                }
+                break;
+            case ConditionType.Pan:
+                Vector2 newPosition = postionValue = LocalPlayer.Instance.GetInputManager().GetCamera().transform.position;
+                if (postionValue != null) {
+                    floatValue += Vector2.Distance(newPosition, postionValue);
+                    if (floatValue >= floatValue2) {
+                        return true;
+                    }
+                }
+                postionValue = newPosition;
+                break;
+            case ConditionType.Zoom:
+                float currentSize = LocalPlayer.Instance.GetInputManager().GetCamera().orthographicSize;
+                if (floatValue2 > floatValue) {
+                    // Zooming in
+                    if (currentSize <= floatValue) return true;
+                } else {
+                    // Zooming out
+                    if (currentSize >= floatValue) return true;
                 }
                 break;
             case ConditionType.Predicate:
