f35f6bae7473e625491e488cfc31b46276e401f4
Oskar Niesen <oskar-github@niesens.com>
2024-07-14 10:42:55 -0500

    ColorPicker prefers higher contrast colors first

--- Assets/Battle/Utils/ColorPicker.cs
+++ Assets/Battle/Utils/ColorPicker.cs
@@ -12,19 +12,36 @@ public class ColorPicker {
         new Color(.4f, 1, 0), // Light green
     };
 
+    private List<Color> preferedColors = new List<Color>();
     private List<Color> availableColors = new List<Color>();
-    public ColorPicker() {
+    /// <summary>
+    /// Creates a new ColorPicker which manages selecting unique colors from a pre-determined pool.
+    /// If all colors in the pre-determined pool have been used then it will pick from a duplicate pool.
+    /// </summary>
+    /// <param name="preferBetterColors">Determines if higher contrast colors should be prefered over lower contrast colors.</param>
+    public ColorPicker(bool preferBetterColors = true) {
         availableColors = new List<Color>(colors);
+        if (preferBetterColors) {
+            preferedColors = new List<Color>() { Color.red, Color.green, Color.yellow, Color.cyan, Color.magenta };
+            preferedColors.ForEach(c => availableColors.Remove(c));
+        }
     }
 
-    public Color pickColor() {
-        if (availableColors.Count == 0) {
-            availableColors.AddRange(colors);
+    public Color PickColor() {
+        if (preferedColors.Count > 0) {
+            int index = Random.Range(0, preferedColors.Count);
+            Color temp = preferedColors[index];
+            preferedColors.RemoveAt(index);
+            return temp;
+        } else {
+            if (availableColors.Count == 0) {
+                availableColors.AddRange(colors);
+            }
+            int index = Random.Range(0, availableColors.Count);
+            Color temp = availableColors[index];
+            availableColors.RemoveAt(index);
+            return temp;
         }
-        int index = Random.Range(0, availableColors.Count);
-        Color temp = availableColors[index];
-        availableColors.RemoveAt(index);
-        return temp;
     }
 
 }
