0e538ac5c7738c59c1b0462a3a98b8382e125db0
Oskar Niesen <oskar-github@niesens.com>
2024-09-07 22:33:33 -0500

    Added Moq dependency

---
+++ Assets/Tests/Battle/Units/ModuleSystem/ModuleSystemTests.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using Moq;
+using NUnit.Framework;
+using UnityEngine;
+
+public class ModuleSystemTests {
+    
+    [Test]
+    public void SetupFromScriptableObject() {
+    }
+}
