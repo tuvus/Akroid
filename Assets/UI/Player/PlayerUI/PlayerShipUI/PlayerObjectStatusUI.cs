f7310fab309cce03e09e9c6a071289ff3929fee8
Oskar Niesen <oskarniesen1@gmail.com>
2025-02-11 21:12:08 -0600

    Improved object status UI

--- Assets/UI/Player/PlayerUI/PlayerShipUI/PlayerObjectStatusUI.cs
+++ Assets/UI/Player/PlayerUI/PlayerShipUI/PlayerObjectStatusUI.cs
@@ -7,16 +7,24 @@ public class PlayerObjectStatusUI : MonoBehaviour {
     [SerializeField] TMP_Text healthText;
     [SerializeField] TMP_Text shieldText;
     [SerializeField] TMP_Text nameText;
+    [SerializeField] private GameObject healthObject;
+    [SerializeField] private GameObject shieldObject;
+
 
     public void RefreshPlayerObjectStatusUI(BattleObjectUI battleObjectUI, int objectCount) {
         objectImage.enabled = true;
         objectImage.sprite = battleObjectUI.GetSprite();
         objectImage.SetNativeSize();
         float sizeRatio = objectImage.rectTransform.sizeDelta.y / objectImage.rectTransform.sizeDelta.x;
-        objectImage.rectTransform.sizeDelta = new Vector2(70 / sizeRatio, 70);
-        if (battleObjectUI.battleObject.IsUnit())
+        objectImage.rectTransform.sizeDelta = new Vector2(100 / sizeRatio, 100);
+        if (battleObjectUI.battleObject.IsUnit()) {
             RefreshUIForUnit((UnitUI)battleObjectUI);
-        else objectImage.color = battleObjectUI.GetSpriteColor();
+        } else {
+            healthObject.SetActive(false);
+            shieldObject.SetActive(false);
+            objectImage.color = battleObjectUI.GetSpriteColor();
+        }
+
         if (objectCount <= 1) {
             nameText.GetComponent<TMP_Text>().text = battleObjectUI.battleObject.objectName;
         } else {
@@ -27,11 +35,13 @@ public class PlayerObjectStatusUI : MonoBehaviour {
     }
 
     private void RefreshUIForUnit(UnitUI unitUI) {
-        // objectImage.color = new Color(unit.GetUnitSelection().GetColor().r, unit.GetUnitSelection().GetColor().g, unit.GetUnitSelection().GetColor().b, 1);
+        healthObject.SetActive(true);
+        shieldObject.SetActive(true);
+        objectImage.color = new Color(unitUI.unitSelection.GetColor().r, unitUI.unitSelection.GetColor().g, unitUI.unitSelection.GetColor().b, 1);
         healthText.GetComponent<TMP_Text>().text = "Hull " + NumFormatter.ConvertNumber(unitUI.unit.GetHealth()) + "/" +
-                                                   NumFormatter.ConvertNumber(unitUI.unit.GetMaxHealth());
+            NumFormatter.ConvertNumber(unitUI.unit.GetMaxHealth());
         shieldText.GetComponent<TMP_Text>().text = "Shields " + NumFormatter.ConvertNumber(unitUI.unit.GetShields()) + "/" +
-                                                   NumFormatter.ConvertNumber(unitUI.unit.GetMaxShields());
+            NumFormatter.ConvertNumber(unitUI.unit.GetMaxShields());
     }
 
     public void RefreshPlayerObjectStatusUI(FleetUI fleetUI, UnitUI unitUI, int unitCount) {
@@ -42,9 +52,9 @@ public class PlayerObjectStatusUI : MonoBehaviour {
         float sizeRatio = objectImage.rectTransform.sizeDelta.y / objectImage.rectTransform.sizeDelta.x;
         objectImage.rectTransform.sizeDelta = new Vector2(70 / sizeRatio, 70);
         healthText.GetComponent<TMP_Text>().text = "Hull " + NumFormatter.ConvertNumber(fleetUI.fleet.GetFleetHealth()) + "/" +
-                                                   NumFormatter.ConvertNumber(fleetUI.fleet.GetMaxFleetHealth());
+            NumFormatter.ConvertNumber(fleetUI.fleet.GetMaxFleetHealth());
         shieldText.GetComponent<TMP_Text>().text = "Shields " + NumFormatter.ConvertNumber(fleetUI.fleet.GetFleetShields()) + "/" +
-                                                   NumFormatter.ConvertNumber(fleetUI.fleet.GetMaxFleetShields());
+            NumFormatter.ConvertNumber(fleetUI.fleet.GetMaxFleetShields());
         nameText.GetComponent<TMP_Text>().text = fleetUI.fleet.GetFleetName() + " (" + unitCount + ")";
         gameObject.SetActive(true);
     }
