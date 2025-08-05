407dd51d65eff61a9ba5995dce569fffbe2e162b
Oskar Niesen <oskarniesen1@gmail.com>
2025-08-05 11:12:43 -0500

    Improved module positioning

--- Assets/UI/Player/PlayerUI/PlayerObjectUI.cs
+++ Assets/UI/Player/PlayerUI/PlayerObjectUI.cs
@@ -35,9 +35,8 @@ public class PlayerObjectUI : PlayerUIMenu<ObjectUI> {
             }
             moduleUIs[i].SetActive(true);
             moduleUIs[i].GetComponent<RectTransform>().anchoredPosition = module.GetPosition() *
-                displayedImageTransform.GetComponent<RectTransform>().sizeDelta * unit.scale *
-                ((unit.IsStation() ? -3f : -.2f) * unit.unitScriptableObject.sprite.pixelsPerUnit) /
-                unit.unitScriptableObject.spriteBounds;
+                displayedImageTransform.GetComponent<RectTransform>().rect.size * 6f *
+                unit.unitScriptableObject.sprite.pixelsPerUnit / 10000;
             moduleUIs[i].transform.GetChild(0).eulerAngles = new Vector3(0, 0, module.rotation);
             if (module.componentScriptableObject.sprite != null) {
                 moduleUIs[i].transform.GetChild(0).GetComponent<Image>().sprite =
